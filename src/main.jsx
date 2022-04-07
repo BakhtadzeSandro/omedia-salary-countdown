bd19199e6ccbc08cc2101e63599802c3e05b522d
Temuri Takalandze <takalandzet@gmail.com>
2022-04-07 16:26:10 +0400

    feat: add PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+  },
+  onOfflineReady() {
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Omedia Salary Countdown",
+        short_name: "Omedia Salary Countdown",
+        description: "Omedia Salary Countdown",
+        theme_color: "#f5322d",
+        icons: [
+          {
+            src: "favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png"
+          },
+          {
+            src: "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png"
+          },
+        ]
+      }
+    })
+  ]
+});
